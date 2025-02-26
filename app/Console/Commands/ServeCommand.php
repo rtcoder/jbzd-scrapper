bda40e3347e048dcfe319b321f4eb2f8e5d1aaa4
Dawid Jeż <dawidjez@gmail.com>
2025-02-26 19:41:39 +0100

    Refactor ServeCommand and streamline bootstrap file
    Refactor `ServeCommand` by adding `set_time_limit(0)` and removing unused idle timeout, optimizing local server handling. Simplify and clean up `bootstrap/app.php` by reducing redundant comments and streamlining bindings and configurations. These changes enhance clarity and maintainability.

--- app/Console/Commands/ServeCommand.php
+++ app/Console/Commands/ServeCommand.php
@@ -3,16 +3,18 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Symfony\Component\Process\Process;
 use Symfony\Component\Process\PhpExecutableFinder;
+use Symfony\Component\Process\Process;
 
 class ServeCommand extends Command
 {
     protected $signature = 'serve';
     protected $description = 'Start Lumen server on localhost:9000';
 
-    public function handle()
+    public function handle(): void
     {
+        set_time_limit(0);
+
         $phpBinary = (new PhpExecutableFinder())->find();
         $command = ["$phpBinary", "-S", "localhost:9000", "-t", "public"];
 
@@ -20,7 +22,6 @@ class ServeCommand extends Command
 
         $process = new Process($command);
         $process->setTimeout(null); // Bez limitu czasu
-        $process->setIdleTimeout(null);
         $process->run(function ($type, $buffer) {
             echo $buffer;
         });
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,76 +1,31 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+use App\Console\Kernel;
+use App\Exceptions\Handler;
+use Illuminate\Contracts\Console\Kernel as AbstractKernel;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+use Laravel\Lumen\Application;
+use Laravel\Lumen\Bootstrap\LoadEnvironmentVariables;
 
-(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
-    dirname(__DIR__)
-))->bootstrap();
+require_once __DIR__ . '/../vendor/autoload.php';
+
+(new LoadEnvironmentVariables(dirname(__DIR__)))->bootstrap();
 
 date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| Here we will load the environment and create the application instance
-| that serves as the central piece of this framework. We'll use this
-| application as an "IoC" container and router for this framework.
-|
-*/
+$app = new Application(dirname(__DIR__));
 
-$app = new Laravel\Lumen\Application(
-    dirname(__DIR__)
-);
+$app->withFacades();
 
-// $app->withFacades();
+$app->withEloquent();
 
-// $app->withEloquent();
+$app->singleton(ExceptionHandler::class, Handler::class);
 
-/*
-|--------------------------------------------------------------------------
-| Register Container Bindings
-|--------------------------------------------------------------------------
-|
-| Now we will register a few bindings in the service container. We will
-| register the exception handler and the console kernel. You may add
-| your own bindings here if you like or you can make another file.
-|
-*/
-
-$app->singleton(
-    Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
-);
-
-/*
-|--------------------------------------------------------------------------
-| Register Config Files
-|--------------------------------------------------------------------------
-|
-| Now we will register the "app" configuration file. If the file exists in
-| your configuration directory it will be loaded; otherwise, we'll load
-| the default version. You may register other files below as needed.
-|
-*/
+$app->singleton(AbstractKernel::class, Kernel::class);
 
 $app->configure('app');
 
-/*
-|--------------------------------------------------------------------------
-| Register Middleware
-|--------------------------------------------------------------------------
-|
-| Next, we will register the middleware with the application. These can
-| be global middleware that run before and after each request into a
-| route or middleware that'll be assigned to some specific routes.
-|
-*/
 
 // $app->middleware([
 //     App\Http\Middleware\ExampleMiddleware::class
@@ -80,36 +35,19 @@ $app->configure('app');
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
 
-/*
-|--------------------------------------------------------------------------
-| Register Service Providers
-|--------------------------------------------------------------------------
-|
-| Here we will register all of the application's service providers which
-| are used to bind services into the container. Service providers are
-| totally optional, so you are not required to uncomment this line.
-|
-*/
-
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
-/*
-|--------------------------------------------------------------------------
-| Load The Application Routes
-|--------------------------------------------------------------------------
-|
-| Next we will include the routes file so that they can all be added to
-| the application. This will provide all of the URLs the application
-| can respond to, as well as the controllers that may handle them.
-|
-*/
-
 $app->router->group([
     'namespace' => 'App\Http\Controllers',
 ], function ($router) {
-    require __DIR__.'/../routes/web.php';
+    require __DIR__ . '/../routes/web.php';
 });
+
+$app->singleton(ViewFactory::class, function ($app) {
+    return $app->make('view');
+});
+
 
 return $app;
